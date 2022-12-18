063b9c65ba6b162d2c4c1ce2a493e844d2f085a2
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-12-18 23:53:41 +0000

    Use poetry.

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-pipenv sync --dev
+poetry config virtualenvs.in-project true
+poetry env use 3.10
+poetry install
 
-git config --global --add safe.directory /workspaces/MusicScience37
-pipenv run pre-commit install
+git config --global --add safe.directory $(pwd)
+poetry run pre-commit install
 
 git config gpg.program gpg2
 git config commit.gpgsign true
 git config tag.gpgsign true
+
+git lfs install
 
 echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
